ae6a206c2af559e1d94c85a6ee7192656de52f60
4bitid <228927605+4bitid@users.noreply.github.com>
2025-12-07 16:38:40 +0900

    add: 初期表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,55 @@
 <script setup lang="ts">
-const { data: data, pending, console.error } = await useFetch('/data/data.json');
+// import { useRoute } from "vue-router"
+// const route = useRoute()
+// console.log("Current path:", route.fullPath)
+
+const { data: psychubes, pending, error } = await useFetch("/data/psychubes.json")
+const sortedPsychubes = computed(() => {
+  const list = psychubes.value || []
+
+  return [...list].sort((a, b) => {
+    const rarityDiff = b.rarity - a.rarity
+
+    if (rarityDiff !== 0) return rarityDiff
+
+    return b.id - a.id
+  })
+})
 
 // Metadata Settinigs
 useSeoMeta({
-  title: '',
-  description: '',
+  title: "【リバース：1999】心相一覧 - Reverse:1999 Psychubes Sort",
+  description: "リバース1999の星6心相を一覧から性能別にソートすることができます。",
 })
 </script>
 
 <template>
-  <div>
-    <h1>Title</h1>
-  </div>
+  <main>
+    <h1>リバース1999：心相一覧</h1>
+
+    <section>s</section>
+
+    <div>
+      <p>{{ psychubes?.length || 0 }} 件のデータを読み込みました。</p>
+
+      <p v-if="pending">データ読み込み中...</p>
+
+      <p v-else-if="error">
+        エラーが発生しました。<br />
+        エラー詳細: {{ error.message }}
+      </p>
+
+      <div v-else>
+        <article v-for="psychube in sortedPsychubes" :key="psychube.id">
+          <img :src="`/images/psychubes/icon-${psychube.id}.png`" :alt="psychube.name" />
+          <h2>{{ psychube.name }}</h2>
+          <p>レアリティ: ★{{ psychube.rarity }} / ID: {{ psychube.id }}</p>
+        </article>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* ここにCSSを記述します */
+</style>
