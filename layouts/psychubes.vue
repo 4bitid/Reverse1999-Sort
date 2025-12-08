20a81b21889fa4aba89781d164b71fccd9f70723
4bitid <228927605+4bitid@users.noreply.github.com>
2025-12-08 19:09:00 +0900

    add: Footer追加

--- layouts/psychubes.vue
+++ layouts/psychubes.vue
@@ -2,4 +2,12 @@
   <div class="min-h-screen bg-psychubes">
     <slot />
   </div>
+  <footer class="bg-gray-900 p-10">
+    <p class="text-xs text-center">
+      本コンテンツは
+      <a href="https://res1999.huijiwiki.com/wiki/" target="_blank" class="text-c-accent">Reverse:1999 中国語Wiki</a>
+      を参考に作成しています。<br />
+      原著作物は CC BY-NC-SA 3.0 ライセンスの下で提供されています。
+    </p>
+  </footer>
 </template>
