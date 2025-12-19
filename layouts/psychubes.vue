eb05b57c355b09c3c6fb5a2f000f305dd6bbd056
4bitid <228927605+4bitid@users.noreply.github.com>
2025-12-19 23:02:11 +0900

    add: Footerに作成者追加

--- layouts/psychubes.vue
+++ layouts/psychubes.vue
@@ -3,11 +3,14 @@
     <slot />
   </div>
   <footer class="bg-gray-900 p-10">
-    <p class="text-xs text-center">
-      本コンテンツは
-      <a href="https://res1999.huijiwiki.com/wiki/" target="_blank" class="text-c-accent">Reverse:1999 中国語Wiki</a>
-      を参考に作成しています。<br />
-      原著作物は CC BY-NC-SA 3.0 ライセンスの下で提供されています。
-    </p>
+    <div class="text-xs text-center">
+      <p>
+        本コンテンツは
+        <a href="https://res1999.huijiwiki.com/wiki/" target="_blank" class="text-c-accent">Reverse:1999 中国語Wiki</a>
+        を参考に作成しています。<br />
+        原著作物は CC BY-NC-SA 3.0 ライセンスの下で提供されています。
+      </p>
+      <p class="mt-5">Creator: <a href="https://bsky.app/profile/4bitid.bsky.social" target="_blank">꒰⍤꒱⁴ᵇ</a></p>
+    </div>
   </footer>
 </template>
